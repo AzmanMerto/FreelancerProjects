a845d6b696b1f80c342f69ba054f58e0b48c558b
NomoteteS <nomotetes.onetrue@icloud.com>
2023-02-14 01:58:46 +0200

    TOOF // created files

---
+++ TOOF Audio/TOOF Audio/TOOF_AudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOOF_AudioApp.swift
+//  TOOF Audio
+//
+//  Created by NomoteteS on 21.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TOOF_AudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
