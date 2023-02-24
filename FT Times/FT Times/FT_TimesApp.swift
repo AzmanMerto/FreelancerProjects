71bbff3c38ccf28eb0de1a8be17927a45affea84
NomoteteS <nomotetes.onetrue@icloud.com>
2023-02-24 12:05:00 +0200

    FT Times // created files

---
+++ FT Times/FT Times/FT_TimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FT_TimesApp.swift
+//  FT Times
+//
+//  Created by NomoteteS on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FT_TimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
