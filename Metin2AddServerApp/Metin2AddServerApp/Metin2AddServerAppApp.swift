54324ee92b9a7760dd467d9d3fc086d744c438bd
NomoteteS <nomotetes.onetrue@icloud.com>
2023-03-15 20:59:23 +0200

    Metin2ServerAddApp // created files

---
+++ Metin2AddServerApp/Metin2AddServerApp/Metin2AddServerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metin2AddServerAppApp.swift
+//  Metin2AddServerApp
+//
+//  Created by NomoteteS on 15.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Metin2AddServerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
