b670cb8fdecf2c1f6639808ef4efba61d58221b0
NomoteteS <nomotetes.onetrue@icloud.com>
2023-03-18 16:18:14 +0200

    Metin2Hanesi // fix: fixed before commit

--- Metin2AddServerApp/Pods/Target Support Files/Pods-Metin2AddServerApp/Pods-Metin2AddServerApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Metin2AddServerApp : NSObject
-@end
-@implementation PodsDummy_Pods_Metin2AddServerApp
-@end
--- Metin2Hanesi/Pods/Target Support Files/Pods-Metin2Hanesi/Pods-Metin2Hanesi-umbrella.h
+++ Metin2Hanesi/Pods/Target Support Files/Pods-Metin2Hanesi/Pods-Metin2Hanesi-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_Metin2AddServerAppVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Metin2AddServerAppVersionString[];
+FOUNDATION_EXPORT double Pods_Metin2HanesiVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Metin2HanesiVersionString[];
 
