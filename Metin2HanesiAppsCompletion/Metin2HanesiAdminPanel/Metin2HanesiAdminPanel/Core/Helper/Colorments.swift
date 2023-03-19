240740645c6d2e057c0e2057deeaace3edd49009
NomoteteS <nomotetes.onetrue@icloud.com>
2023-03-19 13:45:23 +0200

    Metin2Hanesi(admin) // chore: add sdk firebase

---
+++ Metin2HanesiAppsCompletion/Metin2HanesiAdminPanel/Metin2HanesiAdminPanel/Core/Helper/Colorments.swift
@@ -0,0 +1,14 @@
+//
+//  Colorments.swift
+//  Metin2HanesiAdminPanel
+//
+//  Created by NomoteteS on 19.03.2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let MetinBackground: Color = Color("1D2127")
+    static let MetinBlack: Color = Color("242526")
+    static let MetinYellow: Color = Color("D89B2E")
+}
